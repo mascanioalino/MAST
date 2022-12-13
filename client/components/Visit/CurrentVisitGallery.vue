a79b918ce8d94480fd0f9a4d2a2e97d798c9f4ae
trudypainter <35875567+trudypainter@users.noreply.github.com>
2022-12-13 13:01:13 -0500

    made mobile friendly

--- client/components/Visit/CurrentVisitGallery.vue
+++ client/components/Visit/CurrentVisitGallery.vue
@@ -3,9 +3,10 @@
 
 <template>
   <main>
-    <Gallery 
+    <h1>Works Collected In Your Current Visit</h1>
+    <Gallery
       :works="$store.state.visitWorks"
-      :showRemoveWork="true" 
+      :showRemoveWork="true"
       visitDate="Current Visit"
     />
   </main>
@@ -17,7 +18,16 @@ import Gallery from "@/components/Visit/Gallery.vue";
 export default {
   name: "CurrentVisitGallery",
   components: {
-    Gallery
-  }
+    Gallery,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  width: fit-content;
+  padding: 10px;
+  text-align: center;
+  margin: 24px auto;
+}
+</style>
